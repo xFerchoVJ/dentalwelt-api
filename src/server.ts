61336e5e46c09e402c819f7dfff4cf6626ae96a6
xFerchoVJ <fer_elsword@hotmail.com>
2024-03-05 20:39:49 -0600

    Added morgan for logs

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,21 @@
 import express, { Application, NextFunction, Request, Response } from "express";
 import { config } from "dotenv";
-import userRouter from './routes/userRoutes';
+import morgan from "morgan";
+import userRouter from "./routes/userRoutes";
 config();
 
 const app: Application = express();
 
 //Middlewares
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
 //Routes
-app.use('/api/users', userRouter);
+app.use("/api/users", userRouter);
 app.use((req: Request, res: Response, next: NextFunction) => {
-  const error = new Error('Ruta no encontrada');
-  res.status(404).json({ error: error.message })
-})
+  const error = new Error("Ruta no encontrada");
+  res.status(404).json({ error: error.message });
+});
 
 export default app;
